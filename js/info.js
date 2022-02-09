c3ea5b28ee02ba426af4d3161e8def3387a5a59a
adhamjon04 <adhamjonqayumov@gmail.com>
2022-02-09 22:18:32 +0500

    site end

---
+++ js/info.js
@@ -0,0 +1,44 @@
+let elInfoBtn = document.querySelector('.info-btn');
+
+function handleBack(evt) {
+
+    window.location = 'index.html';
+
+}
+elInfoBtn.addEventListener('click', handleBack)
+
+
+let elChange = document.querySelector('.js-change')
+
+let elLet = localStorage.getItem('lat')
+let elLon = localStorage.getItem('lon')
+
+
+
+let curTem = document.querySelector('.js-temp')
+let elCloud = document.querySelector('.js-clear')
+let elIcon = document.querySelector('.js-icon')
+let elSpeed = document.querySelector('.js-speed')
+let elHumidity = document.querySelector('.js-namlik')
+let elPresure = document.querySelector('.js-bosim')
+
+
+
+fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${elLet}&lon=${elLon}&appid=04c7dd78fbebe9dd7ec2423cd0071891`
+    )
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        elChange.textContent = '🏁' + " " + data.name
+        elTemp = data.main.temp;
+        curTem.textContent = "🌡" + 'Harorat' + ' - ' + (elTemp - 273.15).toFixed(2) + ' ' + '℃';
+        elCloud.textContent = "🌤" + 'Holat' + ' - ' + data.weather[0].description;
+        elIcon.id = data.weather[0].id;
+        elIcon.textContent = "☀️" + "🌤" + "⛅️" + "🌥" + "☁️" + "🌦" + "🌧" + "⛈" + "🌩" + "🌨" + "❄️" + "🌬" + "💨" + "💧";
+        elSpeed.textContent = "💨  " + 'Shamol tezligi' + ' - ' + data.wind.speed + '-' + 'km/h';
+        elHumidity.textContent = "💦 " + 'Humidity' + "-" + data.main.humidity + "%"
+        elPresure.textContent = "Pressure  " + " -> " + data.main.pressure + "  Pa"
+    })
+
+// shu sayt
---
+++ js/main.js
@@ -0,0 +1,59 @@
+let elList = document.querySelector('.js-list');
+
+function handleList(evt) {
+    fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${evt.target.textContent}&limit=5&appid=04c7dd78fbebe9dd7ec2423cd0071891`
+        )
+        .then((res) => res.json())
+    localStorage.setItem('lat', evt.target.dataset.lat)
+    localStorage.setItem('lon', evt.target.dataset.lon)
+    window.location = '/info.html'
+}
+elList.addEventListener('click', handleList)
+
+
+//Input Listener
+
+let elInput = document.querySelector('.js-input')
+let elDiv = document.querySelector('.js-div')
+
+function handleBtnClick(evt) {
+
+    localStorage.setItem('lat', evt.target.dataset.lat)
+    localStorage.setItem('lon', evt.target.dataset.lon)
+    window.location = '/info.html'
+
+}
+
+
+
+
+function handleInput(evt) {
+    elDiv.innerHTML = null;
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${evt.target.value}&limit=5&appid=04c7dd78fbebe9dd7ec2423cd0071891`)
+        .then((res) => res.json())
+        .then((data) => data.forEach(value => {
+                console.log(value.lat);
+                let elButton = document.createElement('button')
+                let elItem = document.createElement('li')
+                elButton.textContent = value.name
+                elItem.append(elButton)
+                elDiv.append(elItem)
+
+                elItem.classList.add('btn-site')
+                elButton.classList.add('btn-map')
+
+                elButton.dataset.lat = value.lat;
+                elButton.dataset.lon = value.lon;
+
+
+                elButton.addEventListener('click', handleBtnClick)
+
+            })
+
+
+
+        )
+}
+
+elInput.addEventListener('input', handleInput)
